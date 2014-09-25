0237e399e7fef97823fdb34a8eb05af89a9871a6
AlexSikia <alexandre.sikiaridis@epfl.alumni.ch>
2014-09-25 09:53:33 +0200

    Implements Numeric Values other than 0. ~Alex

--- 1-arithmetic/src/main/scala/fos/Arithmetic.scala
+++ 1-arithmetic/src/main/scala/fos/Arithmetic.scala
@@ -9,7 +9,7 @@ import scala.util.parsing.input._
  *  the TAPL book.
  */
 object Arithmetic extends StandardTokenParsers {
-  lexical.reserved ++= List("true", "false", "0", "if", "then", "else", "succ", "pred", "iszero")
+  lexical.reserved ++= List("true", "false", "if", "then", "else", "succ", "pred", "iszero")
 
   import lexical.NumericLit
 
@@ -23,18 +23,20 @@ object Arithmetic extends StandardTokenParsers {
    * | 'iszero' Expr
    */
   def Expr: Parser[Term] = (
-    //   ... To complete ... 
     "true" ^^^ True
     | "false" ^^^ False
     | ("if" ~> Expr) ~ ("then" ~> Expr) ~ ("else" ~> Expr) ^^ { case e1 ~ e2 ~ e3 => If(e1, e2, e3) }
-    | "0" ^^^ Zero
+    | numericLit ^^ { case n => intToTerm(n.toInt) }
     | "succ" ~> Expr ^^ { case e1 => Succ(e1) }
-    | "pred" ~> Expr ^^ { case e1 => Pred(e1) }
+    | "pred" ~ Expr ^^ { case "pred" ~e1 => Pred(e1) }
     | "iszero" ~> Expr ^^ { case e1 => IsZero(e1) }
     | failure("illegal start of expression"))
   
-    
-  //   ... To complete ... 
+
+  def intToTerm(n: Int): Term = n match {
+    case 0 => Zero
+    case _ => Succ(intToTerm(n-1))
+  }
 
   def reduce(y: Term) = y match {
     case True => True
@@ -52,7 +54,7 @@ object Arithmetic extends StandardTokenParsers {
     val tokens = new lexical.Scanner(StreamReader(new java.io.InputStreamReader(System.in)))
     phrase(Expr)(tokens) match {
       case Success(trees, _) =>
-        
+        println(trees.eval)
       case e =>
         println(e)
     }
